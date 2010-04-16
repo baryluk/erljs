29c715eede3842ece7a9b723d7fd5bd2d18f73f4
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2010-04-16 21:28:22 +0200

    Add handling of other arguments than numbers in the argument list of function. Add unittest for eterm_decode with about 30 example simple and complex terms. Add function which will be run after document load, to initialize vm and run tests.

--- erljs_control.js
+++ erljs_control.js
@@ -10,19 +10,34 @@ function test_json(code) {
 	return test(e);
 }
 
-
-
-
 function erl(X) {
 	var r = /^([^:]+):([^(]+)\((.*)\)$/;
 	var m = r.exec(X);
 	if (m) {
+/*
 		var args = m[3].length==0 ? [] : m[3].split(/,/);
 		var arity = args.length;
 		for (var i = 0; i < arity; i++) {
 			args[i] = new Number(args[i]);
 		}
-		return erljs_vm_call(all_modules, [m[1], m[2], arity], args);
+*/
+
+		var args = [];
+		var A = 0;
+		if (m[3].length) {
+			var i = 0;
+			var a = get_next(m[3],i,false);
+			args[A++]=a[0];
+			i=a[1];
+			while (m[3][i] == ",") {
+				i++;
+				var a = get_next(m[3],i,false);
+				args[A++]=a[0];
+				i=a[1];
+			}
+			if (i != m[3].length) throw "bad syntax in arguments list";
+		}
+		return erljs_vm_call(all_modules, [m[1], m[2], A], args);
 	} else {
 		throw "bad syntax: " + X;
 	}
@@ -67,3 +82,72 @@ function go() {
 	document.getElementById('codeform').innerText = "Result: "+R.toString(); //Object.toJSON(R);
 }
 
+function unittest__term_decode(){
+function d2(x,d2) {
+	var e = eterm_decode(x);
+	//debug(Object.toJSON(e));
+	var o = (d2==e.toString());
+	debug(x+" INPUT");
+	if (!o) {
+	debug(e+" OUTPUT");
+	debug(d2+" EXPECTED")
+	}
+	debug((o ? "OK" : "FAIL FAIL FAIL"));
+	if (!o) throw "FAIL in decoding";
+}
+function d(x) {
+	d2(x,x);
+}
+
+d("{}");
+d("{{}}");
+d("{{{}}}");
+d("{{},{}}");
+d("{{},{},{}}");
+d("[]");
+d("[[]]");
+d("[[[]]]");
+d("[[],[]]");
+d("[[],[],[]]");
+
+d("[{}]");
+d("{[]}");
+d("[{[]}]");
+d("{[{}]}");
+d("[{},{}]");
+d("{[],[]}");
+d("[{},{},{}]");
+d("{[],[],[]}");
+
+d("[[],{}]");
+d("[{},[]]");
+d("{[],{}}");
+d("{{},[]}");
+
+d("[{}|{}]");
+
+d2("[{}|[]]", "[{}]");
+d2("[{}|[{}|[]]]", "[{},{}]");
+
+d("[123,321,512]");
+d("[123,321|-512]");
+
+d("123");
+d("12.3");
+d("0.3");
+d("-0.3");
+d2("011.3e-23", "1.13e-22");
+d2("+123", "123");
+d("-123");
+d("-123.0312");
+d2("{-123,0.1,12,2,1,31,2,000031,23,0.001,3.0,12013.033,5,6.0e+00021,1,78,8,213123,4,12,3,12323}",
+	"{-123,0.1,12,2,1,31,2,31,23,0.001,3,12013.033,5,6e+21,1,78,8,213123,4,12,3,12323}");
+d("{20,1,2,1,[2,12,3],{[2,3],[3|4]}}");
+d("[20,1,2,1,[2,12,3]|{[2,3],[3|4]}]");
+}
+
+
+function erljs_init() {
+	erljs_vm_init(all_modules);
+	unittest__term_decode();
+}
