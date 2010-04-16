124fa3189156b172b4fede31e533bf5ed5e65096
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2010-04-16 21:06:31 +0200

    Added some new modules which will work almost immiedietly (no external dependencies, no messageing, not references, no new processes, no pors or OS dependencies, practically no system erlang:* BIF called).

--- erljs_code.js
+++ erljs_code.js
@@ -1,7 +1,17 @@
 /* Copyritgh 2008 Witold Baryluk. Special thanks to Michal Kolarz, author of IL2JS freamwork */
 
 var all_modules = [
-	["example",  module_example],
-	["lists",  module_lists],
+	["random", module_random],
+	["lists", module_lists],
+	["orddict", module_orddict],
+	["ordsets", module_ordsets],
+	["string", module_string],
+	["queue", module_queue],
+	["dict", module_dict],
+	["proplists", module_proplists],
+	["sets", module_sets],
+	["gb_trees", module_gb_trees],
+	["gb_sets", module_gb_sets],
+	["example", module_example],
 ];
 
