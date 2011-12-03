4f7455f73e45a3126c464ee7cf3625546c7d0a5a
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2011-12-04 00:40:29 +0100

    Few more updates to make_links_for_core_otp_modules.sh
    Add some progress info, remove debuging, check if we are in
    top dir of erljs, and if not exit with proper error message.

--- tools/make_links_for_core_otp_modules.sh
+++ tools/make_links_for_core_otp_modules.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-set -e
-set -v
-
 MODULES="random string proplists dict gb_trees gb_sets orddict ordsets queue sets regexp"
 MODULES="${MODULES} sys proc_lib gen gen_event gen_fsm gen_server supervisor supervisor_bridge"
 # Note: do not add 'lists' module here. it needs to be patched at the end
+
+if [ ! -d "./erl_lib_core" ]; then
+	echo "Please run ./tools/make_links_for_core_top_module.sh from top directory of erljs" >&2
+	exit 1
+fi
 
 if [ ! -d "./erl_lib_core/stdlib" ]; then
 	mkdir "./erl_lib_core/stdlib"
@@ -17,6 +19,7 @@ if [ ! -d "./erl_lib_core/erts" ]; then
 	mkdir "./erl_lib_core/erts"
 fi
 
+echo "Creating links for modules from stdlib application" >&2
 for MODULE in ${MODULES};
 do
 	if [ -L "./erl_lib_core/stdlib/${MODULE}.erl" ]; then
@@ -25,6 +28,7 @@ do
 	ln -v -s /usr/lib/erlang/lib/stdlib-*/src/${MODULE}.erl "./erl_lib_core/stdlib/"
 done
 
+echo "Creating links for modules from kernel application" >&2
 MODULES2="error_logger "
 for MODULE in ${MODULES2};
 do
@@ -34,6 +38,7 @@ do
 	ln -v -s /usr/lib/erlang/lib/kernel-*/src/${MODULE}.erl "./erl_lib_core/kernel/"
 done
 
+echo "Creating links for modules from erts application" >&2
 # usefull for get_argument/1, etc. 
 MODULES3="otp_ring0 init erl_prim_loader erlang"
 for MODULE in ${MODULES3};
@@ -45,5 +50,8 @@ do
 done
 
 
-cp -i /usr/lib/erlang/lib/stdlib-*/src/lists.erl "./erl_lib_core/stdlib/lists.erl"
+echo "Copying and patching lists.erl from stdlib application (it should be safe to overwrite existing file)" >&2
+cp -v -i /usr/lib/erlang/lib/stdlib-*/src/lists.erl "./erl_lib_core/stdlib/lists.erl"
 (cd ./erl_lib_core/stdlib/; patch -p0 < ../lists-erl.patch)
+
+exit 0
