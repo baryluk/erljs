a986833642128253043f328e0588c7996dda44c2
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2011-12-04 00:36:13 +0100

    Update make_links_for_core_otp_modules.sh to use new directories
    It now create links to the erl_lib_core/{kernel,stdlib,erts}/ subdirectories,
    and also patches lists.erl module.
    
    It is also now much more safer, by more quoted arguments,
    and checking of preconditions.
    
    It should be called from main directory of project
     using ./tools/make_links_for_core_otp_modules.sh

--- tools/make_links_for_core_otp_modules.sh
+++ tools/make_links_for_core_otp_modules.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
 
+set -e
+set -v
+
 MODULES="random string proplists dict gb_trees gb_sets orddict ordsets queue sets regexp"
 MODULES="${MODULES} sys proc_lib gen gen_event gen_fsm gen_server supervisor supervisor_bridge"
+# Note: do not add 'lists' module here. it needs to be patched at the end
 
-#lists # lists.erl is slightly different
+if [ ! -d "./erl_lib_core/stdlib" ]; then
+	mkdir "./erl_lib_core/stdlib"
+fi
+if [ ! -d "./erl_lib_core/kernel" ]; then
+	mkdir "./erl_lib_core/kernel"
+fi
+if [ ! -d "./erl_lib_core/erts" ]; then
+	mkdir "./erl_lib_core/erts"
+fi
 
 for MODULE in ${MODULES};
 do
-	if [ -L "${MODULE}.erl" ]; then
-		rm "${MODULE}.erl"
+	if [ -L "./erl_lib_core/stdlib/${MODULE}.erl" ]; then
+		rm "./erl_lib_core/stdlib/${MODULE}.erl"
 	fi
-	ln -v -s /usr/lib/erlang/lib/stdlib-*/src/${MODULE}.erl
+	ln -v -s /usr/lib/erlang/lib/stdlib-*/src/${MODULE}.erl "./erl_lib_core/stdlib/"
 done
 
 MODULES2="error_logger "
 for MODULE in ${MODULES2};
 do
-	if [ -L "${MODULE}.erl" ]; then
-		rm "${MODULE}.erl"
+	if [ -L "./erl_lib_core/kernel/${MODULE}.erl" ]; then
+		rm "./erl_lib_core/kernel/${MODULE}.erl"
 	fi
-	ln -v -s /usr/lib/erlang/lib/kernel-*/src/${MODULE}.erl
+	ln -v -s /usr/lib/erlang/lib/kernel-*/src/${MODULE}.erl "./erl_lib_core/kernel/"
 done
 
 # usefull for get_argument/1, etc. 
 MODULES3="otp_ring0 init erl_prim_loader erlang"
 for MODULE in ${MODULES3};
 do
-	if [ -L "${MODULE}.erl" ]; then
-		rm "${MODULE}.erl"
+	if [ -L "./erl_lib_core/erts/${MODULE}.erl" ]; then
+		rm "./erl_lib_core/erts/${MODULE}.erl"
 	fi
-	ln -v -s /usr/lib/erlang/lib/erts-*/src/${MODULE}.erl
+	ln -v -s /usr/lib/erlang/lib/erts-*/src/${MODULE}.erl "./erl_lib_core/erts/"
 done
 
 
-cp -i /usr/lib/erlang/lib/stdlib-*/src/lists.erl lists.erl
-patch -p0 < lists-erl.patch
+cp -i /usr/lib/erlang/lib/stdlib-*/src/lists.erl "./erl_lib_core/stdlib/lists.erl"
+(cd ./erl_lib_core/stdlib/; patch -p0 < ../lists-erl.patch)
