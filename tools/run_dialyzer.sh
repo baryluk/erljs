6ee964336337f693f826d73143b8869227484e73
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2011-12-04 05:40:14 +0100

    Updates in dialyzer script.
    Break long lines in dialyzer script.
    Change from bash to sh,
    update to use new subdirectories
    and add copyright info

--- tools/run_dialyzer.sh
+++ tools/run_dialyzer.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-dialyzer --build_plt --apps erts kernel stdlib mnesia compiler crypto hipe inets parsetools sasl xmerl debugger common_test eunit edoc docbuilder tools syntax_tools ssh wx ssl snmp gs test_server runtime_tools webtool observer public_key et
+# Copyright 2009-2011, Witold Baryluk <baryluk@smp.if.uj.edu.pl>
+# erljs project
 
-#dialyzer erljs_tests.erl  erljs.erl  json_simple.erl 
+dialyzer --build_plt --apps erts kernel stdlib mnesia compiler \
+	crypto hipe inets parsetools sasl xmerl debugger common_test \
+	eunit edoc docbuilder tools syntax_tools ssh wx ssl snmp gs \
+	test_server runtime_tools webtool observer public_key et
+
+cd erl_src
+dialyzer erljs_tests.erl erljs.erl json_simple.erl
