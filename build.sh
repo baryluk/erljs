7fc218346e67663f2ef693b350fa54106ea413fb
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2011-12-15 21:53:28 +0100

    Add some post-bulid messages to ./build.sh script
    erljs.html and erljs_code.js may need manual updating, if some new
    modules are needed. erljs.html and erljs_code.js is provided in
    repository, but in future it should be removed, and compiled from
    sources automatically.
    
    In fact module will be loaded/downloaded dynamically, and dynamically
    added to VM, so both of this steps will not be nacassary, but some list
    of pre-loaded module can be helpful (for example, if we know we will use
    lists, erlang, gen_server, gen, proplists anyway, it is good to preload
    them, even in single request to server, or embeded directly somewhere).
    
    For now, build.sh just displays small notice what needs to be manually
    updated and how.

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-exec ./tools/compile_all.escript
+./tools/compile_all.escript
+
+echo
+echo "Build is complete (or not), but still few things needs to be done manually..."
+echo
+echo "You may need to update:"
+echo "    erljs_html/erljs.html (<script src=...> tags, which loads .beam.js files)"
+echo "    js_src/erljs_code.js (nacassary mappings in all_modules array)"
+echo 
+echo "For your conviniance, both <script ...> tags, and all_modules array, was printed above by compiler"
+echo "It was however printed twice, once for main modules, and once for tests."
+echo "Make sure to copie both sets, and merge them in order."
+echo
+echo "In future, both of this steps will be not nacassary (after implementing proper code loader)."
+echo "But make sure they are correct now. If you are compiling just cloned repository,"
+echo "then supplied erljs.html and erljs_code.js files, should be correct."
+echo
