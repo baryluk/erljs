450dc23748f968e6896d732eab64ede5ad0c3876
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2011-12-15 22:27:03 +0100

    Update run_dialyzer.sh script
    Run dialyzer create only if ~/.dialyzer_plt file is missing.
    
    Run dialyzer analysis with new modules, after split of modules from
    erljs.erl to erljs_compiler.erl, erljs_compile_all.erl, erljs.erl, etc.

--- tools/run_dialyzer.sh
+++ tools/run_dialyzer.sh
@@ -3,10 +3,12 @@
 # Copyright 2009-2011, Witold Baryluk <baryluk@smp.if.uj.edu.pl>
 # erljs project
 
+if [ ! -f ~/.dialyzer_plt ]; then
 dialyzer --build_plt --apps erts kernel stdlib mnesia compiler \
 	crypto hipe inets parsetools sasl xmerl debugger common_test \
 	eunit edoc docbuilder tools syntax_tools ssh wx ssl snmp gs \
 	test_server runtime_tools webtool observer public_key et
+fi
 
 cd erl_src
-dialyzer erljs_tests.erl erljs.erl json_simple.erl
+dialyzer json_simple.erl erljs_compiler.erl erljs_compile_all.erl erljs_tests.erl erljs.erl
